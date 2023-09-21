15163c37f87ae31e10d756afa732095f5be10cbd
Sabo <57690741+carloskbonet@users.noreply.github.com>
2023-09-21 11:34:38 -0300

    commit 21.09.2023

---
+++ pages/api/actions/user/create.tsx
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { createUser } from "../../controller/UserController";
+import { userRequest } from "@/request/UserRequest";
+
+export default async (req: NextApiRequest , res: NextApiResponse) => {
+    if ( req.method != 'POST' ) {
+        return res.status(403).json( { message: "Method not allowed" } );
+    }
+
+    const { name , cpf , email , password } = req.body;
+    
+    //Verify request
+
+    const checkRequest = userRequest(email , cpf , password , name);
+    
+    if ( checkRequest.status == false ) {
+        return res.status(403).json( { message: checkRequest.message } );
+    }
+
+    //Create model
+    const response = await createUser(email , cpf , password , name);
+
+    return res.status(200).json( response );
+}
