d155c75ce76fde9a9eaaa25ce7c5f2f3772618f6
Sabo <57690741+carloskbonet@users.noreply.github.com>
2023-09-29 11:23:58 -0300

    commit 29.09.2023

---
+++ prisma/migrations/20230929142158_database/migration.sql
@@ -0,0 +1,71 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT,
+    "cpf" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "movie" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "releaseDate" DATETIME NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "rating" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "value" INTEGER NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "movieId" INTEGER NOT NULL,
+    CONSTRAINT "rating_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "rating_movieId_fkey" FOREIGN KEY ("movieId") REFERENCES "movie" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "genre" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_genreTomovie" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+    CONSTRAINT "_genreTomovie_A_fkey" FOREIGN KEY ("A") REFERENCES "genre" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_genreTomovie_B_fkey" FOREIGN KEY ("B") REFERENCES "movie" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_id_key" ON "user"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_cpf_key" ON "user"("cpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "movie_id_key" ON "movie"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "rating_id_key" ON "rating"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "genre_id_key" ON "genre"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_genreTomovie_AB_unique" ON "_genreTomovie"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_genreTomovie_B_index" ON "_genreTomovie"("B");
