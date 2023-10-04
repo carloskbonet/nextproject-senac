58dca36426c0d113b50adae0bbfe34f095540908
Sabo <57690741+carloskbonet@users.noreply.github.com>
2023-10-04 10:32:23 -0300

    commit 04.10.2023
    using "useEffect"

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { checkToken } from '@/services/tokenConfig';
 import { deleteCookie, getCookie } from 'cookies-next'
 import { Inter } from 'next/font/google'
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from "@/styles/home.module.css";
 
 const inter = Inter({ subsets: ['latin'] })
@@ -10,6 +10,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   const router = useRouter();
   const [name, setName] = useState("");
+  const [data, setData]: any = useState(undefined);
   const [movie, setMovie] = useState({
     name: '',
     releaseDate: '',
@@ -17,15 +18,32 @@ export default function Home() {
     updatedAt: ''
   })
 
+  async function fetchData() {
+    const response = await fetch(`/api/actions/movie/select`, {
+      method: 'GET'
+    });
+
+    const responseJson = await response.json();
+
+    setData(responseJson);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  // Remove login cookies
   function logOut() {
     deleteCookie('authorization');
     router.push(`/user/login`);
   }
 
+  // Link the input field with name constant
   function handleFormEdit(event: any) {
     setName(event.target.value);
   }
 
+  // Form action - server req
   async function formSubmit(event: any) {
     try {
       event.preventDefault();
@@ -62,6 +80,19 @@ export default function Home() {
             value={name} onChange={(evento) => { handleFormEdit(evento) }} />
         </form>
       </div>
+
+      {data != undefined && data instanceof Array ?
+        data.map(item => (
+          <div className={styles.container}>
+            <p>{item.id}</p>
+            <p>{item.name}</p>
+            <p>{item.releaseDate}</p>
+            <p>{item.created_at}</p>
+            <p>{item.updated_at}</p>
+          </div>
+        ))
+        : <p>No movies found</p>
+      }
 
       <div className={styles.container}>
         <p>{movie.name}</p>
