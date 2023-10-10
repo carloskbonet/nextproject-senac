89ba333b3297d18dcde255dd1559e2cd0756973d
Sabo <57690741+carloskbonet@users.noreply.github.com>
2023-10-10 11:35:25 -0300

    Update UserController.tsx

--- pages/api/controller/UserController.tsx
+++ pages/api/controller/UserController.tsx
@@ -1,31 +1,41 @@
 import { generateToken } from '@/services/tokenConfig';
 import { createUserModel, findUserModelByCPF, findUserModelByEmail, findUserModelLogin } from '../model/user';
 
-export async function createUser(_email:string , _cpf:string , _password:string , _name?:string) {
-    const userByCPF = await findUserModelByCPF(_cpf);
+export async function createUser(_email: string, _cpf: string, _password: string, _name?: string) {
+    try {
+        const userByCPF = await findUserModelByCPF(_cpf);
 
-    if (userByCPF != undefined) {
-        return { message: "CPF already registered." };
+        if (userByCPF != undefined) {
+            return { message: "CPF already registered." };
+        }
+
+        const userByEmail = await findUserModelByEmail(_email);
+
+        if (userByEmail != undefined) {
+            return { message: "Email already registered." };
+        }
+
+        const response = await createUserModel(_cpf, _email, _password, _name);
+        return response;
     }
-    
-    const userByEmail = await findUserModelByEmail(_email);
-
-    if (userByEmail != undefined) {
-        return { message: "Email already registered." };
+    catch (err) {
+        return { message: "Something went wrong" };
     }
-
-    const response = await createUserModel(_cpf , _email , _password , _name);
-    return response;
 }
 
-export async function login(_email:string , _password:string) {
-    const userLogin = await findUserModelLogin(_email , _password);
+export async function login(_email: string, _password: string) {
+    try {
+        const userLogin = await findUserModelLogin(_email, _password);
 
-    if (userLogin == undefined) {
-        return { message: "Incorrect email or password" }
+        if (userLogin == undefined) {
+            return { message: "Incorrect email or password" }
+        }
+
+        const _token = generateToken(userLogin.email);
+
+        return { token: _token }
     }
-
-    const _token = generateToken(userLogin.email);
-
-    return { token: _token }
+    catch (err) {
+        return { message: "Something went wrong" };
+    }
 }
